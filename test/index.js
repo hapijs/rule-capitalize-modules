37b1996e64ddaada473680ef00787776c369b276
cjihrig <cjihrig@gmail.com>
2015-08-02 20:01:23 -0400

    update to eslint 1.x.x and use newly exposed rule tester

--- test/index.js
+++ test/index.js
@@ -8,89 +8,81 @@ var lab = exports.lab = Lab.script();
 var expect = Code.expect;
 var describe = lab.describe;
 var it = lab.it;
-var linter = ESLint.linter;
-var linterConfig = {rules: {}, env: {es6: true}}; // ES6 used to test destructuring
+var RuleTester = ESLint.RuleTester;
 
 Code.settings.truncateMessages = false;
-linter.defineRule(HapiCapitalizeModules.esLintRuleName, HapiCapitalizeModules);
 
 describe('ESLint Rule', function() {
   it('reports warning when module is not capitalized', function(done) {
+    var ruleTester = new RuleTester();
     var code = [
       'var hapi = require("hapi");',
       'var poop; poop = require("poop");',
       'var foo = {bar: function() { var hapi = require("hapi"); }};'
     ];
 
-    linterConfig.rules[HapiCapitalizeModules.esLintRuleName] = 1;
-
-    for (var i = 0; i < code.length; ++i) {
-      var result = linter.verify(code[i], linterConfig);
-
-      expect(result).to.be.an.array();
-      expect(result.length).to.equal(1);
-      expect(result[0].ruleId).to.equal(HapiCapitalizeModules.esLintRuleName);
-      expect(result[0].message).to.equal('Imported module variable name not capitalized.');
-      expect(result[0].nodeType).to.match(/VariableDeclarator|AssignmentExpression/);
-    }
-
+    ruleTester.run(HapiCapitalizeModules.esLintRuleName, HapiCapitalizeModules, {
+      valid: [],
+      invalid: code.map(function(code) {
+        return {
+          code: code,
+          errors: [{message: 'Imported module variable name not capitalized.'}]
+        };
+      })
+    });
     done();
   });
 
   it('does not report anything if module variable is capitalized', function(done) {
+    var ruleTester = new RuleTester();
     var code = [
       'var Hapi = require("hapi");',
       'var Poop; Poop = require("poop");',
       'Code = require("code");'
     ];
 
-    linterConfig.rules[HapiCapitalizeModules.esLintRuleName] = 1;
-
-    for (var i = 0; i < code.length; ++i) {
-      var result = linter.verify(code[i], linterConfig);
-
-      expect(result).to.be.an.array();
-      expect(result.length).to.equal(0);
-    }
-
+    ruleTester.run(HapiCapitalizeModules.esLintRuleName, HapiCapitalizeModules, {
+      valid: code.map(function(code) {
+        return {code: code};
+      }),
+      invalid: []
+    });
     done();
   });
 
   it('only warns on globals when global-scope-only is set', function(done) {
-    var code = [
+    var ruleTester = new RuleTester();
+    var valid = [
       'function foo() { var hapi = require("hapi"); }',
       'var foo = function() { var hapi = require("hapi"); }',
       'var foo = {bar: function() { hapi = require("hapi"); }};'
     ];
-
-    linterConfig.rules[HapiCapitalizeModules.esLintRuleName] = [1, 'global-scope-only'];
-
-    for (var i = 0; i < code.length; ++i) {
-      var result = linter.verify(code[i], linterConfig);
-
-      expect(result).to.be.an.array();
-      expect(result.length).to.equal(0);
-    }
-
-    code = [
+    var invalid = [
       'hapi = require("hapi");',
       'var poop; poop = require("poop");'
     ];
 
-    for (var i = 0; i < code.length; ++i) {
-      var result = linter.verify(code[i], linterConfig);
 
-      expect(result).to.be.an.array();
-      expect(result.length).to.equal(1);
-      expect(result[0].ruleId).to.equal(HapiCapitalizeModules.esLintRuleName);
-      expect(result[0].message).to.equal('Imported module variable name not capitalized.');
-      expect(result[0].nodeType).to.match(/VariableDeclarator|AssignmentExpression/);
-    }
-
+    ruleTester.run(HapiCapitalizeModules.esLintRuleName, HapiCapitalizeModules, {
+      valid: valid.map(function(code) {
+        return {
+          code: code,
+          options: ['global-scope-only']
+        };
+      }),
+      invalid: invalid.map(function(code) {
+        return {
+          code: code,
+          options: ['global-scope-only'],
+          errors: [{message: 'Imported module variable name not capitalized.'}]
+        };
+      })
+    });
     done();
   });
 
   it('does not report anything for non-module variables', function(done) {
+    var ruleTester = new RuleTester();
     var code = [
       'var foo, bar, baz;',
       'var foo = fn()',
@@ -102,15 +94,15 @@ describe('ESLint Rule', function() {
       '[foo, bar] = require("baz");'
     ];
 
-    linterConfig.rules[HapiCapitalizeModules.esLintRuleName] = 1;
-
-    for (var i = 0; i < code.length; ++i) {
-      var result = linter.verify(code[i], linterConfig);
-
-      expect(result).to.be.an.array();
-      expect(result.length).to.equal(0);
-    }
-
+    ruleTester.run(HapiCapitalizeModules.esLintRuleName, HapiCapitalizeModules, {
+      valid: code.map(function(code) {
+        return {
+          code: code,
+          ecmaFeatures: {destructuring: true}
+        };
+      }),
+      invalid: []
+    });
     done();
   });
 });
